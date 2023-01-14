1a114f4e304f8bfcf95e92f9731384b090404998
Radu Anca <121979307+radu-a1091@users.noreply.github.com>
2023-01-14 18:45:09 +0000

    added small model + refactored readme

--- README.md
+++ README.md
@@ -4,9 +4,15 @@ A program that calculates and displays the semantic similarity between a given s
 ### Pre-requisites
 - install spacy library - [Link](https://spacy.io/usage)
 - install the english md model (en_core_web_md) - [Link](https://spacy.io/usage#quickstart)
+- install the english sm model (en_core_web_sm) - [Link](https://spacy.io/usage#quickstart) 
 <br/>
 ### Running the program
 Download the files in the repo on your machine, open the semantic.py file with your preferred IDE.
+
+You can change between small model and the medium model by modifying the nlp variable's loading option:
+    
+- to load the small model, change <u>__load()__</u> to <u>__load('en_core_web_sm')__</u>
+- to load the medium model, change <u>__load()__</u> to <u>__load('en_core_web_sm')__</u>
 
 You can change the "sentence_to_compare" variable to any other text you want to compare.
 
--- semantic.py
+++ semantic.py
@@ -1,8 +1,8 @@
 # importing required libraries
 import spacy
 
-#loading the md english model 
-nlp = spacy.load('en_core_web_md')
+#loading the english model 
+nlp = spacy.load('en_core_web_sm')
 
 # declaring the sentence to compare and a dict of sentences to compare with
 sentence_to_compare = "Why is my cat on the car"
@@ -21,7 +21,7 @@ model_sentence = nlp(sentence_to_compare)
 for sentence in sentences:
     similarity = nlp(sentence).similarity(model_sentence)
     print(sentence + " - ", similarity)
-"""Output:
+"""Output from the medium model (en_web_core_md):
 where did my dog go -  0.630065230699739
 Hello, there is my car -  0.8033180111627156
 I've lost my car in my car -  0.6787541571030323
@@ -40,4 +40,17 @@ the similarities results for the first, 3rd and 5th are quite close to each othe
     - 1st and 5th are about dogs; dogs and cats are both pets
     - one of the objects in the 3rd sentence is 'car' which appears twice (as the object lost and its location) which
     is also the location of the lost cat in the the sentence_to_compare
+    
+Output from the smaller model (en_core_web_sm):
+where did my dog go -  0.46559175808905845  
+Hello, there is my car -  0.5229294863903653
+I've lost my car in my car -  0.6350136333854123
+I'd like my boat back -  0.43399616845180666
+I will name my dog Diana -  0.3717855214295043
+
+the clear difference between the sm and the md model is that the sm model is more based on context.
+
+with the sm model, the highest similarity is with the 3rd sentence which is quite inaccurate. the context is similar though,
+as there is something lost and the location has something to do with the "car".
+ 
 """
